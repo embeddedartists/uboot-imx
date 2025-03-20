b6da8f19b142185ca469c2cf79ef5a08cf8f97f4
Anders Lindvall <anders.lindvall@embeddedartists.com>
2025-03-20 13:44:44 +0000

    imx93: Corrected pin init for BT_REG_ON and WL_DEV_WAKE to solve m.2

--- board/embeddedartists/mx93ea-ucom/imx93ea-ucom.c
+++ board/embeddedartists/mx93ea-ucom/imx93ea-ucom.c
@@ -93,7 +93,10 @@ static void board_gpio_init(void)
 	board_gpio_configure_pin("gpio@21_0", "BT_REG_ON", 0);
 	board_gpio_configure_pin("gpio@21_1", "WL_REG_ON", 0);
 	board_gpio_configure_pin("gpio@22_19", "ONBOARD_BT_REG_ON", 0);
-	board_gpio_configure_pin("gpio@22_20", "ONBOARD_WL_REG_ON", 1);
+	board_gpio_configure_pin("gpio@22_20", "ONBOARD_WL_REG_ON", 0);
+
+	/* Pull WL_DEV_WAKE LOW */
+	board_gpio_configure_pin("gpio@22_12", "ONBOARD_WL_DEV_WAKE", 0);
 }
 
 int board_init(void)
